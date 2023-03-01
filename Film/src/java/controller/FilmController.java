89e7bf3854b257e45e7b6f1896a3ac86482fbadd
MioraRatsiresy <miora.ratsiresy@gmail.com>
2023-03-01 13:16:38 +0300

    Update FilmController.java

--- Film/src/java/controller/FilmController.java
+++ Film/src/java/controller/FilmController.java
@@ -26,42 +26,60 @@ import org.springframework.web.multipart.MultipartFile;
  */
 @Controller
 public class FilmController {
+
     @Autowired
     HibernateDao dao;
-    
+
     @RequestMapping("/index")
-    public String index(){
+    public String index() {
         return "index";
     }
+
     @RequestMapping("/formulairescene")
-    public String scene(Model model){
+    public String scene(Model model) {
         model.addAttribute("plateau", dao.findAll(Plateau.class));
         return "scene";
     }
+
     @RequestMapping("/liste/film")
-    public String listefilm(Model model){
-       /* List<Film> film=dao.findAll(Film.class);
+    public String listefilm(Model model, HttpServletRequest request) {
+        /* List<Film> film=dao.findAll(Film.class);
         for(int i=0;i<film.size();i++){
            System.out.println(film.get(i).getTitre());
         }*/
-        model.addAttribute("film", dao.findAll(Film.class));
+        int npp = 4;
+        int numpage = 1;
+        if (request.getParameter("numpage") != null) {
+            numpage = Integer.parseInt(request.getParameter("numpage"));
+        }
+        int indiceprime = numpage * npp - npp;
+        model.addAttribute("film", dao.paginateWhere(Film.class, indiceprime, npp));
+        model.addAttribute("numpage", numpage);
         return "index";
     }
-    
-     @RequestMapping("/liste/scene/{idfilm}")
-    public String listescene(@PathVariable int idfilm,Model model){
-       /*List<Scene> film=dao.findAll(Scene.class);
+
+    @RequestMapping("/liste/scene/{idfilm}")
+    public String listescene(@PathVariable int idfilm, Model model, HttpServletRequest request) {
+        /*List<Scene> film=dao.findAll(Scene.class);
         for(int i=0;i<film.size();i++){
            System.out.println(film.get(i).getDescriptionscene());
         }*/
-        Scene scene=new Scene();
+        Scene scene = new Scene();
         scene.setIdfilm(idfilm);
-        model.addAttribute("scene", dao.findAll(Film.class));
+        int npp = 4;
+        int numpage = 1;
+        if (request.getParameter("numpage") != null) {
+            numpage = Integer.parseInt(request.getParameter("numpage"));
+        }
+        int indiceprime = numpage * npp - npp;
+        model.addAttribute("scene", dao.paginateWhere(scene, indiceprime, npp));
+        model.addAttribute("numpage", numpage);
         return "index";
     }
+
     @RequestMapping("/insertFilm")
     public String insertFilm(@RequestParam("sary") MultipartFile file, HttpServletRequest request) {
-        Film film=new Film();
+        Film film = new Film();
         try {
             String fileName = file.getOriginalFilename();
             System.out.println("NOM DE FICHIER : " + fileName);
@@ -70,27 +88,27 @@ public class FilmController {
             film.setSary(fileName);
             film.setTitre(request.getParameter("titre"));
             film.setDescriptionfilm(request.getParameter("description"));
-            
+
             dao.create(film);
-            
+
         } catch (Exception e) {
-           e.printStackTrace();
+            e.printStackTrace();
         }
         return "redirect:/liste/film";
     }
-    
-     @RequestMapping("/insertScene/{idfilm}")
-    public String insertScene(@PathVariable int idfilm,HttpServletRequest request) {
-        Scene scene=new Scene();
+
+    @RequestMapping("/insertScene/{idfilm}")
+    public String insertScene(@PathVariable int idfilm, HttpServletRequest request) {
+        Scene scene = new Scene();
         try {
             scene.setIdauteur(Integer.parseInt(request.getParameter("auteur")));
             scene.setIdfilm(idfilm);
             scene.setIdplateau(Integer.parseInt(request.getParameter("idplateau")));
             scene.setDescriptionscene(request.getParameter("description"));
             dao.create(scene);
-            
+
         } catch (Exception e) {
-           e.printStackTrace();
+            e.printStackTrace();
         }
         return "scene";
     }
