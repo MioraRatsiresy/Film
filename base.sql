452f80e2883978770f25b5e583996922b9095e1a
MioraRatsiresy <99732281+MioraRatsiresy@users.noreply.github.com>
2023-03-21 10:41:37 +0300

    Update base.sql

--- base.sql
+++ base.sql
@@ -16,6 +16,9 @@ DROP TABLE IF EXISTS Personnage CASCADE;
 DROP TABLE IF EXISTS Scene CASCADE;
 DROP TABLE IF EXISTS actionScene CASCADE;
 DROP TABLE IF EXISTS DetailsAction CASCADE;
+DROP TABLE IF EXISTS Indisponibiliteplateau CASCADE;
+DROP TABLE IF EXISTS JourFerie CASCADE;
+DROP TABLE IF EXISTS Planning CASCADE;
 
 create table Film(
 	id serial primary key , 
@@ -24,6 +27,7 @@ create table Film(
 	descriptionfilm text		
 );
 
+INSERT INTO Film values(default,'Bobby','photo.png','kaka');
 
 create table Administrateur(
 	id serial primary key,
@@ -39,14 +43,13 @@ create table auteur(
 	motdeapasse varchar(20)
 );
 
+insert into auteur values (default,'Mitasoa','Celestin','mitasoa@gmail.com','zety')
 
 create table auteurfilm(
 	id serial primary key,
 	idfilm int references film(id),
 	idauteur int references auteur(id)
 );
-ALTER TABLE auteurfilm ADD FOREIGN KEY(idfilm) REFERENCES Film(id);
-ALTER TABLE auteurfilm ADD FOREIGN KEY(idauteur) REFERENCES Auteur(id);
 	
 	
 create table vetement(
@@ -89,9 +92,6 @@ create table Personnage(
 	prenom varchar(50),
 	idRole int references rolePersonnage(id)
 	);
-
-ALTER TABLE Personnage ADD FOREIGN KEY(idRole) REFERENCES rolePersonnage(id);
-
 	
 
 	
@@ -103,11 +103,6 @@ create table scene (
 	idplateau int references plateau(id),
 	etatscene int default 0  -- 0 crée 4 validé 6 planifie
 	);
-ALTER TABLE scene ADD FOREIGN KEY(idfilm) REFERENCES Film(id);
-ALTER TABLE scene ADD FOREIGN KEY(idauteur) REFERENCES Auteur(id);
-ALTER TABLE scene ADD FOREIGN KEY(idplateau) REFERENCES plateau(id);
-
-
 
 CREATE VIEW SceneView as
 SELECT Scene.*,auteur.nom,auteur.prenom,plateau.plateau from scene join auteur on auteur.id=scene.idauteur join plateau on plateau.id=scene.idplateau; 
@@ -117,8 +112,6 @@ create table actionScene(
 	descriptionaction varchar(100),	
 	idscene int references scene(id)
 );
-ALTER TABLE actionScene ADD FOREIGN KEY(idscene) REFERENCES Scene(id);
-
 
 create table DetailsAction(
 	id serial primary key,
@@ -131,13 +124,6 @@ create table DetailsAction(
 	idvetement int references vetement(id),
 	sketch text 	
 );
-
-ALTER TABLE DetailsAction ADD FOREIGN KEY(idaction) REFERENCES actionScene(id);
-ALTER TABLE DetailsAction ADD FOREIGN KEY(idexpression) REFERENCES expression(id);
-ALTER TABLE DetailsAction ADD FOREIGN KEY(idsentiment) REFERENCES sentiment(id);
-ALTER TABLE DetailsAction ADD FOREIGN KEY(idton) REFERENCES Ton(id);
-ALTER TABLE DetailsAction ADD FOREIGN KEY(idpersonnage) REFERENCES Personnage(id);
-
 
 create table Indisponibiliteplateau(
 	idplateau int not null,
@@ -153,11 +139,33 @@ create table JourFerie(
 );
 
 
+create table Planning(
+	dateplanning date,
+	heureplanning time,
+	scene int
+);
+
 insert into  sentiment(sentiment) values('bonheur'), ('chagrin'), ('colère'), ('compassion'), ('crainte'),('dégoût'),('désespoir'), ('effroi'), ('ennui'),('envie'),('frayeur'), ('fureur'), ('gaieté'), ('haine'), ('honte'), ('jalousie'),('joie'), ('mélancolie'), ('mépris'), ('panique'), ('peine'), ('peur'), ('rage'), ('respect'), ('stupeur'), ('surprise'), ('terreur'), ('tristesse');
 
--- insert into expression(expression) values(''),(
 
 insert into ton(ton) values ('Lamentable'),(' langoureux'), ('languissant'), ('larmoyant'), ('lasse'), ('leger'), ('lent'), ('leste'), ('lugubre'),('Magistral'), ('majestueux'), ('maniéré'), ('martial'), ('maussade'), ('méchant'), ('mélancolique'), ('mélodramatique'), ('menaçant'), ('méprisant'), ('mesuré'), ('mielleux'), ('militaire'), ('modere'), ('modeste'), ('monocorde'), ('monotone'), ('montant'), ('moqueur'), ('moralisateur'), ('mordant'), ('mourant');
+
+
+create or replace function planning(film int)
+returns table(
+	dateplanning date,
+	heureplanning time,
+	scene int
+)
+language plpgsql
+as
+$$
+  declare
+    begin
+
+    end;
+$$;
+
 
 	
 	
